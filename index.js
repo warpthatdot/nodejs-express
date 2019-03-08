1a8715e3edf86c6bd8b21ba36760a8cf7a753e66
warpthatdot <44891716+warpthatdot@users.noreply.github.com>
2019-03-08 12:53:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   return res.send('Received a POST HTTP method');
 });
+app.put('/user/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
 
 module.exports = app
